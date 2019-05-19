7c7800fa9e197f65183ac72ea9f2433d554fe0f8
Alex Monteil <alex.monteil@outlook.com>
2019-05-18 23:18:54 -0500

    changing to ES6 syntax

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var body = document.querySelector('body');
-var h3 = document.querySelector('h3');
-var randombutton = document.querySelector('button');
-color1 = document.querySelectorAll('input')[0];
-color2 = document.querySelectorAll('input')[1];
+const body = document.querySelector('body');
+const h3 = document.querySelector('h3');
+const randombutton = document.querySelector('button');
+const color1 = document.querySelectorAll('input')[0];
+const color2 = document.querySelectorAll('input')[1];
 
-function changeBackground() {
+const changeBackground = () => {
     body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
     h3.textContent = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')' + ';';
 }
 
-function randomBackground() {
+const randomBackground = () => {
     color1.value = ('#' + Math.floor(Math.random() * 16777216).toString(16)).toString();
     color2.value = ('#' + Math.floor(Math.random() * 16777216).toString(16)).toString();
     changeBackground();
