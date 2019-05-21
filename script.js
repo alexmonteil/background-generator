d7fd5cc04cd669901e08f1dc95f364837eae65ce
Alex Monteil <alex.monteil@outlook.com>
2019-05-21 12:15:03 -0500

    added descriptions for functions

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const randombutton = document.querySelector('button');
 const color1 = document.querySelectorAll('input')[0];
 const color2 = document.querySelectorAll('input')[1];
 
+//Changes background to colors selected & writes the CSS property as H3
 const changeBackground = () => {
     body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
     h3.textContent = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')' + ';';
 }
 
+//Generates random colors and changes background to those
 const randomBackground = () => {
     color1.value = ('#' + Math.floor(Math.random() * 16777216).toString(16)).toString();
     color2.value = ('#' + Math.floor(Math.random() * 16777216).toString(16)).toString();
